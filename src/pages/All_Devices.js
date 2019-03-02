d6623af715bbb94fc8bd75aa80522d4cfee330a3
Aravind Jaimon <aravindjaimon1@gmail.com>
2019-03-03 03:51:36 +0530

    search pattern fixed

--- src/pages/All_Devices.js
+++ src/pages/All_Devices.js
@@ -3,15 +3,22 @@ import DevicesCard from "../components/Devices_Card";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import { Link } from "react-router-dom";
+import classnames from "classnames";
+
 import {
   Row,
   Col,
   InputGroup,
   Input,
   InputGroupAddon,
-  Button
+  Button,
+  NavItem,
+  Nav
 } from "reactstrap";
 import AllResultsLayout from "../components/DeviceListLayout";
+import TabPane from "reactstrap/lib/TabPane";
+import TabContent from "reactstrap/lib/TabContent";
+import NavLink from "reactstrap/lib/NavLink";
 
 const GET_ALL_DEVICES = gql`
   query {
@@ -21,13 +28,38 @@ const GET_ALL_DEVICES = gql`
     }
   }
 `;
+const GET_ALL_COMPONENTS = gql`
+  query {
+    allComponent {
+      id
+      name
+    }
+  }
+`;
 
 export default class All_Devices extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      activeTab: "1"
+    };
+  }
+
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
 
   render() {
     return (
       <AllResultsLayout>
         <div className="container mt-4">
+          {/* Search Bar */}
           <Row className="py-2 ">
             <Col>
               <InputGroup size="lg">
@@ -40,25 +72,81 @@ export default class All_Devices extends Component {
               </InputGroup>
             </Col>
           </Row>
-          <h2>Devices</h2>
-          <Query query={GET_ALL_DEVICES}>
-            {({ loading, error, data }) => {
-              if (loading) return <p>Loading...</p>;
-              if (error) return <p>Error :(</p>;
-              return (
-                <div className="row m-auto">
-                  {data.allDevice.map(single_device => (
-                    <Link
-                      className="link_effect"
-                      to={`/device_details/${single_device.id}`}
-                    >
-                      <DevicesCard {...single_device} />
-                    </Link>
-                  ))}
-                </div>
-              );
-            }}
-          </Query>
+          {/* Navigation Tabs */}
+          <Nav tabs>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: this.state.activeTab === "1" })}
+                onClick={() => {
+                  this.toggle("1");
+                }}
+              >
+                Components
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: this.state.activeTab === "2" })}
+                onClick={() => {
+                  this.toggle("2");
+                }}
+              >
+                Devices
+              </NavLink>
+            </NavItem>
+          </Nav>
+          {/* Tabular Data */}
+          <TabContent activeTab={this.state.activeTab}>
+            {/* Components Tab */}
+            <TabPane tabId="1">
+              <Row>
+                <Col>
+                  <Query query={GET_ALL_COMPONENTS}>
+                    {({ loading, error, data }) => {
+                      if (loading) return <p>Loading...</p>;
+                      if (error) return <p>Error :(</p>;
+                      return (
+                        <div className="row m-auto">
+                          {data.allComponent.map(single_device => (
+                            <Link
+                              className="link_effect"
+                              to={`/device_details/${single_device.id}`}
+                            >
+                              <DevicesCard {...single_device} />
+                            </Link>
+                          ))}
+                        </div>
+                      );
+                    }}
+                  </Query>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="2">
+              <Row>
+                <Col>
+                  <Query query={GET_ALL_DEVICES}>
+                    {({ loading, error, data }) => {
+                      if (loading) return <p>Loading...</p>;
+                      if (error) return <p>Error :(</p>;
+                      return (
+                        <div className="row m-auto">
+                          {data.allDevice.map(single_device => (
+                            <Link
+                              className="link_effect"
+                              to={`/device_details/${single_device.id}`}
+                            >
+                              <DevicesCard {...single_device} />
+                            </Link>
+                          ))}
+                        </div>
+                      );
+                    }}
+                  </Query>
+                </Col>
+              </Row>
+            </TabPane>
+          </TabContent>
         </div>
       </AllResultsLayout>
     );
